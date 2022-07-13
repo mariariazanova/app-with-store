3b35a7eb9d254fda6d8debd6f90bf9dea4e57fd5
Maria Riazanova <m.ryazanova@itransition.com>
2022-07-13 22:39:17 +0200

    Delete unnecessary component

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './components/form/form.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AddressComponent } from "./components/address/address.component";
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { GeneralComponent } from "./components/general/general.component";
 import { SourceComponent } from './components/source/source.component';
@@ -13,7 +12,6 @@ import { CodesComponent } from "./components/codes/codes.component";
   declarations: [
     GeneralComponent,
     FormComponent,
-    AddressComponent,
     NavigationComponent,
     SourceComponent,
     DayOffsComponent,
@@ -24,6 +22,6 @@ import { CodesComponent } from "./components/codes/codes.component";
     ReactiveFormsModule,
     FormsModule,
   ],
-  exports: [FormComponent, AddressComponent],
+  exports: [FormComponent],
 })
 export class ProjectModule { }
